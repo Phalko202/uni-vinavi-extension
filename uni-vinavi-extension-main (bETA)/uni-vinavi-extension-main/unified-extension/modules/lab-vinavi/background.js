0c5a1481f68ca504dc302b303b29be1c74d4e88f
Phalko202 <phalko200@gmail.com>
2026-02-26 12:06:54 +0500

    Delete uni-vinavi-extension-main (bETA)/uni-vinavi-extension-main/unified-extension/modules/lab-vinavi directory

--- uni-vinavi-extension-main (bETA)/uni-vinavi-extension-main/unified-extension/modules/lab-vinavi/background.js
+++
@@ -1,8 +0,0 @@
-// background.js - Simple service worker for HMH Lab Extension
-
-// Open dashboard when extension icon is clicked
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL('dashboard.html')
-  });
-});
